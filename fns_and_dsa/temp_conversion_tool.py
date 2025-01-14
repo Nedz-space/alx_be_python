97a0908dcb5a8b1f82f62560e26381bde33dcfc8
Nedz-space <0xnedz@gmail.com>
2025-01-14 10:52:53 +0100

    Add and write script for temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,7 +1,7 @@
 # Define global conversion factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
-FREEZING_POINT_DIFFERENCE =32
+FREEZING_POINT_DIFFERENCE=32
 
 # Function to convert Fahrenheit to Celsius
 def convert_to_celsius(fahrenheit):
